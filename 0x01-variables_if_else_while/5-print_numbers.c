f93ff3a3306a1fb89e408dede964c7c20a5418bf
Mark Lelei <zemlelei@gmail.com>
2022-03-12 02:45:41 +0300

    Added task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function
+ * Description: Prints all single digit numbers of base 10 from 0
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+	putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
