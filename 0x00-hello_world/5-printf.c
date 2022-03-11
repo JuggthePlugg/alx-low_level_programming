a7392d129711fc7a41067a8e084f85bafda977ed
Mark Lelei <zemlelei@gmail.com>
2022-03-11 03:44:04 +0300

    Added a script that prints a line

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+
 /**
- * main - function
+ * main - prints the phrase
  * Return - 0
  */
+
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
