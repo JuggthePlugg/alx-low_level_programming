840daeeba6aa1576a5c7c40ca3339e5fb81cf13a
Mark Lelei <zemlelei@gmail.com>
2022-03-12 02:10:56 +0300

    Added task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function 
+ * Description: Prints letters of the alphabet in lowercase
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	for(i = 'a',i <= 'z', i++)
+	{
+	putchar(i);
+	}
+	putchar('\n');
+	return 0;
+}
