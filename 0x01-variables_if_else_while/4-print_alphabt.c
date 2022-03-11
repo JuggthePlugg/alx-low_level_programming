a17f9f47bdb31149108da6f52d9c941f50882318
Mark Lelei <zemlelei@gmail.com>
2022-03-12 02:40:23 +0300

    Added task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Description: Prints the alphabet in lowercase apart from q and e
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	if(i != 'q' && i!= 'e')
+	{
+	putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
