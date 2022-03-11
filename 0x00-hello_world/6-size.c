3529276df5512cfcdc93b10fa6df5e00a573c3a1
Mark Lelei <zemlelei@gmail.com>
2022-03-11 04:05:39 +0300

    Added a script that prints various types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /*
- * main - prints the size of various types
- * Return: 0 if exited correctly
+ *main - prints the size of various types
+ *Return: 0 if exited correctly
  *
  */
-int main (void)
+int main(void)
 {
 printf("Size of a char: %d byte(s)\n", sizeof(char));
 printf("Size of an int: %d byte(s)\n", sizeof(int));
