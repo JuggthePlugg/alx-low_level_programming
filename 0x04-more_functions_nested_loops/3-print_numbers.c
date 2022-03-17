28b0c11f0419a0b05646ec63de881ee14b94728d
Mark Lelei <zemlelei@gmail.com>
2022-03-17 20:15:37 +0300

    Added task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - function to print 0 to 9 then newline
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0' ; i <= '9' ; i++)
+		_putchar(i);
+	_putchar('\n');
+}
