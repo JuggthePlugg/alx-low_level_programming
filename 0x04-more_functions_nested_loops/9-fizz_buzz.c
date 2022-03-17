5b96145c1a8ffaef11db2a04e8b0be44b9174afe
Mark Lelei <zemlelei@gmail.com>
2022-03-18 01:50:30 +0300

    Added task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,28 +1,42 @@
 #include <stdio.h>
-#include "main.h"
-
 /**
- * main - FizzBuzz
- * Return: 0
+ * main - check the code alx School students.
+ *
+ * Return: void.
  */
 
 int main(void)
 {
-	int i;
 
-	for (i = 1 ; i <= 100 ; i++)
+	int i = 1;
+
+	while (i <= 100)
 	{
-		if (i % 15 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
+		{
 			printf("FizzBuzz");
+		}
 		else if (i % 3 == 0)
+		{
 			printf("Fizz");
+		}
 		else if (i % 5 == 0)
+		{
 			printf("Buzz");
+		}
 		else
-			printf("%d", i);
-		if (i < 100)
-			printf(" ");
-		else
-			printf("\n");
+		{
+			printf("%i", i);
+		}
+
+		if (i != 100)
+		{
+			putchar(' ');
+		}
+
+		i++;
 	}
+	putchar('\n');
 	return (0);
+
+}
