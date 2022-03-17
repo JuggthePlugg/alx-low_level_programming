b2c309727d876380cd0f4989e5508fcf809b64b0
Mark Lelei <zemlelei@gmail.com>
2022-03-17 19:37:02 +0300

    Added task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _isupper - function
+* Description: checks for uppercase character
+* Return: 1 if c is uppercase
+* otherwise Return 0
+*/
+int _isupper(int c)
+{
+        if (c >= 'A' && c <= 'Z' )
+            return (1);
+        else
+            return (0);
+}
