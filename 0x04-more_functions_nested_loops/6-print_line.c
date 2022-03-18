8bd361cd02974cef50bd51171c23e069425cb088
Mark Lelei <zemlelei@gmail.com>
2022-03-18 03:27:51 +0300

    Added task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
- *
  * print_line - check for a digit
  * @n : number of _ to be printed
  * Return:void
@@ -18,4 +17,5 @@ void print_line(int n)
 		i++;
 	}
 	_putchar('\n');
+
 }
