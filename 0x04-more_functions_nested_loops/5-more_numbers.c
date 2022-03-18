1fe8f961d0350bcb638e374bfaba7828899566a1
Mark Lelei <zemlelei@gmail.com>
2022-03-18 03:21:29 +0300

    Added task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *  * more_numbers - prints more numbers
- *   * Return:void
- *    */
+ * more_numbers - prints more numbers
+ * Return:void
+ */
 
 void more_numbers(void)
 {
@@ -13,17 +13,19 @@ void more_numbers(void)
 
 	while (i < 10)
 	{
-			for (n = 0; n <= 14; n++)
+		for (n = 0; n <= 14; n++)
+		{
+			c = n;
+			if (n > 9)
 			{
-						c = n;
-						if (n > 9)
-						{
-							_putchar('1');
-							c = n % 10;
-						}
-						_putchar('0' + c);
+				_putchar('1');
+				c = n % 10;
 			}
-			_putchar('\n');
-			i++;
+
+			_putchar('0' + c);
 		}
+
+		_putchar('\n');
+		i++;
+	}
 }
