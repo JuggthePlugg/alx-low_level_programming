ecbc37163532a183f369403b72fb0475be143c32
Mark Lelei <zemlelei@gmail.com>
2022-04-15 01:24:57 +0300

    All tasks

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,4 +20,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		ptr = va_arg(ap, char *);
-
+		if (ptr != NULL)
+			printf("%s", ptr);
+		else
+			printf("(nil)");
+		if (i < (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ap);
+}
